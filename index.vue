082a0175f5ed959606ed6e728a1496cc9a6320d8
Robert Kowalski <rgkmg25@gmail.com>
2023-02-10 23:46:54 -0500

    Update index.vue

--- index.vue
+++ index.vue
@@ -1,7 +1,14 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+
+const cards = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 </script>
 
 <template>
   <slot/>
+  <div style="background-color: green; width: 100%;">
+  </div>
 </template>
+
+<script scoped>
+</script>

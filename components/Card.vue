eb21571874691c6f074b62863d3cc3c108156a0d
Robert Kowalski <rgkmg25@gmail.com>
2023-02-25 23:43:17 -0500

    Create Card.vue

---
+++ components/Card.vue
@@ -0,0 +1,23 @@
+ 19 lines (16 sloc) 384 Bytes
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const props = defineProps({
+  value: {
+    type: Number,
+    default: 0
+  },
+  rank: {
+    type: String
+  }
+});
+</script>
+
+<template>
+  <div style="background-color: white; width: 100%; border-radius: 10px;">
+    <slot></slot>
+  </div>
+</template>
+
+<script scoped>
+</script>
